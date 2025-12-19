4c13b43559a5d43091c06d77b3853f6808d5b3c8
Breno Farias da Silva <breno.vicio78@gmail.com>
2025-12-19 11:38:41 -0300

    REFACTOR: Rename 'Steam Achievement Abuser Loop' → 'Steam Achievement Abuser Multiple Runs'

---
+++ src/Steam Achievement Abuser Multiple Runs/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Steam Achievement Abuser Multiple Runs")]
+[assembly: AssemblyDescription("Steam Achievement Abuser Multiple Runs")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("sa68ru.xyz")]
+[assembly: AssemblyProduct("Steam Achievement Abuser Multiple Runs")]
+[assembly: AssemblyCopyright("Copyright © sa68ru  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c3d4e5f6-6789-4bcd-9abc-1234567890ab")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
