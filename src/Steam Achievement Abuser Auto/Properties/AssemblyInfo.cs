97c4b21fe38da2330b15c3be71b50aceedbc36d9
Breno Farias da Silva <breno.vicio78@gmail.com>
2025-12-19 11:14:38 -0300

    FEAT: Add 'Steam Achievement Abuser Auto' automated project

---
+++ src/Steam Achievement Abuser Auto/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Steam Achievement Abuser Auto")]
+[assembly: AssemblyDescription("Steam Achievement Abuser Auto")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("sa68ru.xyz")]
+[assembly: AssemblyProduct("Steam Achievement Abuser Auto")]
+[assembly: AssemblyCopyright("Copyright © sa68ru  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a9b3e7d3-5b2f-4c1a-9f3a-1e2d3c4f5a67")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
