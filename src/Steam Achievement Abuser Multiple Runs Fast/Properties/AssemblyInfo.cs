10f7bb7aa52702e5dd705cd6ae75e8762289b826
Breno Farias da Silva <breno.vicio78@gmail.com>
2026-08-16 09:39:32 -0300

    BUILD: Add fast runner assembly metadata

---
+++ src/Steam Achievement Abuser Multiple Runs Fast/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Steam Achievement Abuser Multiple Runs Fast")]
+[assembly: AssemblyDescription("Steam Achievement Abuser Multiple Runs Fast")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("sa68ru.xyz")]
+[assembly: AssemblyProduct("Steam Achievement Abuser Multiple Runs Fast")]
+[assembly: AssemblyCopyright("Copyright (c) sa68ru  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("60085d91-8f3d-4741-9ef1-478cc5c9ead1")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
